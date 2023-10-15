dbe8b31b4c2a56dc36bab5a5550b45113258a672
Horacio Mota <horaciomota93u@gmail.com>
2023-10-15 07:56:20 -0700

    Folders and promotional img added

---
+++ Helthmo/View/SingUp/SingUp.swift
@@ -0,0 +1,20 @@
+//
+//  SingUp.swift
+//  Helthmo
+//
+//  Created by Horacio Mota on 15/10/2023.
+//
+
+import SwiftUI
+
+struct SingUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SingUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SingUp()
+    }
+}
