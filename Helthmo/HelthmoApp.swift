ad55beb83c23a74768c04529b62c1e4258faa155
Horacio Mota <horaciomota93u@gmail.com>
2023-10-15 07:36:39 -0700

    Initial Commit

---
+++ Helthmo/HelthmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelthmoApp.swift
+//  Helthmo
+//
+//  Created by Horacio Mota on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelthmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
